59d10e906e7b9fc0c7241cfaa8cd0a4d59a3c2d8
CakeSlayer00 <iamgroot0087@gmail.com>
2024-04-19 19:03:52 +0500

    bit-of-refactoring

--- Medieval Runner/Assets/Scripts/PlayerController.cs
+++ Medieval Runner/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Security.Cryptography;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,30 +7,46 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private AnimationCurve _xAnimation;
 
     private Rigidbody _rigidbody;
-    private SideSwitchController _switchController;
+
+    private const float AnimationDurtaion = 1f;
+    private const float OffsetX = 3.5f;
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>(); 
-        _switchController = GameObject.Find(nameof(SideSwitchController)).GetComponent<SideSwitchController>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         transform.Translate(Vector3.forward * (Time.deltaTime * _movementSpeed));
-        
+
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             StopAllCoroutines();
-            StartCoroutine(_switchController.SwitchSide(transform, _xAnimation , Side.Right));
+            StartCoroutine(SwitchSide(transform, _xAnimation, Side.Right));
         }
 
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             StopAllCoroutines();
-            StartCoroutine(_switchController.SwitchSide(transform, _xAnimation , Side.Left));
+            StartCoroutine(SwitchSide(transform, _xAnimation, Side.Left));
         }
 
-        if (transform.position.y < -5) { transform.position = Vector3.zero; }
+        if (transform.position.y < -5) transform.position = Vector3.zero;
+    }
+
+    public IEnumerator SwitchSide(Transform obj, AnimationCurve curve, Side side)
+    {
+        var expiredTime = 0f;
+        var startPosition = obj.position;
+
+        while (expiredTime < 1)
+        {
+            expiredTime += Time.deltaTime;
+            var progress = expiredTime / AnimationDurtaion;
+            obj.position = startPosition + new Vector3(curve.Evaluate(progress) * OffsetX * (int)side, 0, 0);
+
+            yield return null;
+        }
     }
 }
---
+++ Medieval Runner/Assets/Scripts/RoadSpawnManager.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawnManager : MonoBehaviour
+{
+    private GameObject _road;
+    private GameObject _player;
+
+    private void Start()
+    {
+        _player = GameObject.Find("Player");
+        _road = GameObject.Find("Road");
+    }
+
+    private void Update()
+    {
+        var playerPositionByZ = _player.transform.position.z;
+
+        if (Mathf.Round(playerPositionByZ) % 50 == 0)
+        {
+            _road.transform.position = new Vector3(0f, 0f, playerPositionByZ);
+        }
+    }
+}
